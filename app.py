4e729b7e7a46ecbed1d1ed5abbb02abc96ca8b09
Your Name <you@example.com>
2023-06-10 04:30:55 +0300

    Added predict price method

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, request
+import joblib
+import numpy
+
+Model_PATH = 'mlmodels/model.pkl'
+SCALER_X_PATH = 'mlmodels/scaler_x.pkl'
+SCALER_Y_PATH = 'mlmodels/scaler_y.pkl'
+
 
 app = Flask(__name__)
+model = joblib.load(Model_PATH)
+sc_x = joblib.load(SCALER_X_PATH)
+sc_y = joblib.load(SCALER_Y_PATH)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/predict_price', methods = ['GET'])
+def predict():  # put application's code here
+    args = request.args
+    open_plan = args.get('open_plan', default=-1, type=int)
+    rooms = args.get('rooms', default=-1, type=int)
+    area = args.get('area', default=-1)
+    renovation =args.get('renovation', defult=-1, type=int)
+
+    x = numpy.array([open_plan, rooms, area, renovation]).reshape(1,-1)
+    x = sc_x.transform(x)
+    result = model.predict(x)
+    result = sc_y.inverse_transform(reshape(1,-1))
+
+    return str(result[0][0])
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5444, host='0.0.0.0')
